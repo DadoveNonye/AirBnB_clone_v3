8f7cd66f081a79874438a0e0a65f5f30eb5d01df
DadoveNonye <98288131+DadoveNonye@users.noreply.github.com>
2024-03-01 03:52:51 +0100

    fixing endpoints

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,26 @@
 """index that imports app_views"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
+@app_views.route('/status', methods=['GET'])
 def status():
     """Returns a JSON response with status OK"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=['GET'])
+def get_stats():
+    '''
+    Endpoint that retrieves the number of each objects by type
+    '''
+    stats = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User')
+    }
+    return jsonify(stats)
+
 
