42159ad258336a95cbe8fa73ee7d5ffaa84855bf
DadoveNonye <98288131+DadoveNonye@users.noreply.github.com>
2024-03-01 04:51:57 +0100

    updated

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,12 +11,9 @@ app = Flask(__name__)
 # enable CORS and allow for origins:
 CORS(app, resources={r'/api/v1/*': {'origins': '0.0.0.0'}})
 
-app.register_blueprint(app_views)
-app.url_map.strict_slashes = False
-
-
 # Register blueprint for API routes
 app.register_blueprint(app_views)
+app.url_map.strict_slashes = False
 
 @app.teardown_appcontext
 def teardown(error):
