8ba389dafd74f111a8b3c32d6377a4df59a86f86
DadoveNonye <98288131+DadoveNonye@users.noreply.github.com>
2024-03-01 04:49:52 +0100

    updated

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
 """creates the app.py"""
 from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 import os
 
 app = Flask(__name__)
+
+# enable CORS and allow for origins:
+CORS(app, resources={r'/api/v1/*': {'origins': '0.0.0.0'}})
+
+app.register_blueprint(app_views)
+app.url_map.strict_slashes = False
+
 
 # Register blueprint for API routes
 app.register_blueprint(app_views)
